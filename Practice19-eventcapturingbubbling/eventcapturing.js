736c13331ffe993d2c0b04ffe404c4786cae051b
Krushna Rohitkumar Patel <Krushna.Patel@igtsolutions.com>
2025-04-04 18:34:41 +0530

    my commit

--- Practice19-eventcapturingbubbling/eventcapturing.js
+++ Practice19-eventcapturingbubbling/eventcapturing.js
@@ -1,12 +1,17 @@
 const parentDiv = document.getElementById("parent");
 const button = document.getElementById("child");
+const button1 = document.getElementById("innerchild");
+
+button1.addEventListener("click", () => {
+  console.log("Button 1 clicked (capturing)");
+});
 
 button.addEventListener(
   "click",
   () => {
-    console.log("Button clicked (capturing");
-  },
-  true
+    console.log("Button clicked (capturing)");
+  }
+  // true
 );
 
 parentDiv.addEventListener(
